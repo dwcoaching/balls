c6f3d19486193bf4f54b4d91e42bb80987aa5aca
Andrew Kolpakov <kolpakov@dwcoaching.ru>
2019-11-17 18:04:02 +0300

    add command line interface and prettier stats

--- balls.py
+++ balls.py
@@ -1,10 +1,19 @@
 from simulator import Simulator
+import sys
 
-simulator = Simulator(10000)
+number_of_tries = int(sys.argv[1])
+simulator = Simulator(number_of_tries)
 stats = simulator.getStats()
+probabilty = stats['white_balls_on_the_first_step']['white_balls_on_the_second_step_count'] / stats['white_balls_on_the_first_step']['count']
 
-print(stats)
-print('\nProbabilty:')
-print(stats['white_balls_on_the_first_step']['white_balls_on_the_second_step_count']
-    / stats['white_balls_on_the_first_step']['count'])
+print('TOTAL NUMBER OF TRIES: ' + str(number_of_tries) + '\n')
+print('WHITE BALLS ON THE FIRST STEP: ' + str(stats['white_balls_on_the_first_step']['count']))
+print('   Second step, white balls: : ' + str(stats['white_balls_on_the_first_step']['white_balls_on_the_second_step_count']))
+print('   Second step, black balls: ' + str(stats['white_balls_on_the_first_step']['black_balls_on_the_second_step_count']))
+print()
+print('BLACK BALLS ON THE FIRST STEP: ' + str(stats['black_balls_on_the_first_step']['count']))
+print('   Second step, white balls: : ' + str(stats['black_balls_on_the_first_step']['white_balls_on_the_second_step_count']))
+print('   Second step, black balls: ' + str(stats['black_balls_on_the_first_step']['black_balls_on_the_second_step_count']))
+print()
+print('PROBABILITY: ' + str(probabilty))
 
