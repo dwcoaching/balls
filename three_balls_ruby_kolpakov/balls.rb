176372a7491e6d665540bc8f320973584b84b8fc
Andrew Kolpakov <kolpakov@dwcoaching.ru>
2019-11-21 00:31:11 +0300

    clean up ruby code

--- three_balls_ruby_kolpakov/balls.rb
+++ three_balls_ruby_kolpakov/balls.rb
@@ -2,29 +2,14 @@ require_relative('constants.rb')
 require_relative('basket.rb')
 require_relative('simulator.rb')
 
-simulator = Simulator.new(10000000)
-
-all_balls = {
-    WHITE => 0,
-    BLACK => 0,
-    RED => 0
-}
-
-simulator.get_baskets.each do |basket|
-    basket.get_balls.each do |ball|
-        all_balls[ball] += 1
-    end
-end
-
-puts all_balls
-
+simulator = Simulator.new(1000000)
 simulator.run
 results = simulator.get_results
 
 probability = results[WHITE][WHITE][RED][:count].to_f / results[WHITE][WHITE][:count].to_f
 
 puts probability
-puts '======='
+puts
 
 balls = [WHITE, BLACK, RED]
 
@@ -38,5 +23,4 @@ balls.each do |first|
             puts '        THIRD ' + third + ' ' + results[first][second][third][:count].to_s
         end
     end
-
 end
